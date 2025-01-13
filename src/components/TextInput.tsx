2d75c4697b155b439b6fb82d3a554cabc9d4d448
GIGAluckman <andrey.a.voronov@gmail.com>
2025-01-13 17:12:14 +0100

    Input pattern add

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,3 +1,4 @@
+import { inputMaterialLoopParams } from '../constants/inputMaterialLoopParams';
 import styles from './TextInput.module.css';
 
 interface TextInputProps {
@@ -18,13 +19,27 @@ export default function TextInput({
 			<label className={styles.label}></label>
 			{label}{' '}
 			<input
+				required={true}
 				name={name}
 				defaultValue={defaultValue || ''}
 				key={`${name}-${defaultValue}`}
 				className={styles.input}
+				inputMode="decimal"
+				pattern={
+					name === inputMaterialLoopParams.ms.name
+						? '[+-]?(?:[1-9]\\d*)(?:\\.\\d+)?(?:[eE][+-]?\\d+)?'
+						: '[+-]?(?:0|[1-9]\\d*)(?:\\.\\d+)?(?:[eE][+-]?\\d+)?'
+				}
+				placeholder="e.g., 2.6e-12"
+				title={
+					'Please enter a valid' +
+					(name === inputMaterialLoopParams.ms.name
+						? ' nonzero'
+						: '') +
+					' decimal number or scientific notation (e.g., 84, 3.4, -2.56, or 1.2e-10).'
+				}
 			/>{' '}
 			{unit}
-			{/* <hr /> */}
 		</p>
 	);
 }
