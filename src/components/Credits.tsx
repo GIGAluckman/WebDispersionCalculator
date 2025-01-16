9f877f9f99df2a6c50e1d1528402df2e733c9ece
GIGAluckman <andrey.a.voronov@gmail.com>
2025-01-16 18:24:16 +0100

    init

---
+++ src/components/Credits.tsx
@@ -0,0 +1,15 @@
+export default function Credits() {
+	return (
+		<div className="text-center mt-3">
+			Powered by{' '}
+			<a
+				href="https://www.tetrax.software"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				{' '}
+				<strong>TetraX</strong>
+			</a>
+		</div>
+	);
+}
