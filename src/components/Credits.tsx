42da220cf62f09a717fb631c5d26aa738cc38d63
GIGAluckman <andrey.a.voronov@gmail.com>
2025-01-21 15:04:28 +0100

    Bigger credits

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -1,15 +1,44 @@
 export default function Credits() {
 	return (
-		<div className="text-center mt-3">
-			Powered by{' '}
-			<a
-				href="https://www.tetrax.software"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				{' '}
-				<strong>TetraX</strong>
-			</a>
+		<div className="d-flex justify-content-center">
+			<div className="text-left mt-3">
+				Powered by{' '}
+				<a
+					href="https://www.tetrax.software"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					{' '}
+					<strong>TetraX</strong>
+				</a>
+				<br />
+				If you are using this program for your research, please cite:
+				<br />
+				<strong>[1]</strong> L. Körber, G. Quasebarth, A. Hempel, F.
+				Zahn, A. Otto, E. Westphal, R. Hertel and A. Kákay (2022),
+				<br />
+				“TetraX: Finite-Element Micromagnetic-Modeling Package”{' '}
+				<a
+					href="https://doi.org/10.14278/rodare.1418"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					DOI
+				</a>
+				<br />
+				<strong>[2]</strong> L. Körber, G. Quasebarth, A. Otto and A.
+				Kákay (2021),
+				<br />
+				“Finite-element dynamic-matrix approach for spin-wave
+				dispersions in magnonic waveguides with arbitrary cross section”{' '}
+				<a
+					href="https://doi.org/10.1063/5.0054169"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					DOI
+				</a>
+			</div>
 		</div>
 	);
 }
