82bda22982482dfc12e8bf9a71076af80502ab5f
GIGAluckman <andrey.a.voronov@gmail.com>
2025-01-31 15:27:14 +0100

    Styles plus status implementation

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
+import styles from './ProgressBar.module.css';
 
 interface ProgressBarProps {
 	simulationId: string;
 }
 
 export default function ProgressBar({ simulationId }: ProgressBarProps) {
-	const [progress, setProgress] = useState(0);
+	const [progress, setProgress] = useState<number>(0);
+	const [status, setStatus] = useState<string>('Beginning simulation');
 	console.log('Simulation ID: ', simulationId);
 
 	useEffect(() => {
@@ -19,7 +21,8 @@ export default function ProgressBar({ simulationId }: ProgressBarProps) {
 				clearInterval(interval);
 				alert('Simulation not found!');
 			} else {
-				setProgress(data.status);
+				setProgress(data.progress);
+				setStatus(data.status);
 			}
 		}, 1000);
 
@@ -27,18 +30,23 @@ export default function ProgressBar({ simulationId }: ProgressBarProps) {
 	}, []);
 
 	return (
-		<div
-			className="progress"
-			role="progressbar"
-			aria-valuenow={progress}
-			aria-valuemin={0}
-			aria-valuemax={1}
-		>
-			<div
-				className="progress-bar"
-				style={{ width: `${progress * 100}%` }}
-			>
-				{progress * 100}%
+		<div className="d-flex justify-content-center">
+			<div className={styles.colprogress}>
+				Simulation status: {status}
+				<div
+					className="progress"
+					role="progressbar"
+					aria-valuenow={progress}
+					aria-valuemin={0}
+					aria-valuemax={1}
+				>
+					<div
+						className="progress-bar"
+						style={{ width: `${progress * 100}%` }}
+					>
+						{(progress * 100).toFixed(0)}%
+					</div>
+				</div>
 			</div>
 		</div>
 	);
