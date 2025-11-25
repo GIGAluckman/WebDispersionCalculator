1642bdc407ffc8b80cc7e018ae13e89c3e80674d
GIGAluckman <andrey.a.voronov@gmail.com>
2025-11-25 16:50:07 +0100

    TetraX logo add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import logoNanoMag from '../assets/logoNanoMag.jpg';
 import logoDispCalc from '../assets/logoDispCalc.png';
+import logoTetraX from '../assets/logoTetraX.png';
 import styles from './styles/Header.module.css';
 
 interface HeaderProps {
@@ -9,18 +10,39 @@ interface HeaderProps {
 interface ImageProps {
 	src: string;
 	alt: string;
+	height?: string;
 }
 
-function Image({ src, alt }: ImageProps) {
-	return <img src={src} alt={alt} className={styles.img} />;
+function Image({ src, alt, height }: ImageProps) {
+	return (
+		<img
+			src={src}
+			alt={alt}
+			className={styles.img}
+			style={{ height: height }}
+		/>
+	);
 }
 
 export default function Header({ title }: HeaderProps) {
 	return (
 		<header className={styles.header + ' d-flex justify-content-center'}>
 			<Image src={logoDispCalc} alt="logoDispCalc" />
-			<h1>{title}</h1>
-			<Image src={logoNanoMag} alt="logoNanoMag" />
+			<h1 className={styles.title}>{title}</h1>
+			<a
+				href="https://nanomag.univie.ac.at"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<Image src={logoNanoMag} alt="logoNanoMag" height="60px" />
+			</a>
+			<a
+				href="https://www.tetrax.software"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<Image src={logoTetraX} alt="logoTetraX" height="45px" />
+			</a>
 		</header>
 	);
 }
